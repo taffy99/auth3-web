755184c28d94cc4bf2b0eaff9f385dee9be22da0
taffy99 <452339112@qq.com>
2023-09-04 11:07:31 +0800

    登录逻辑优化

---
+++ src/store/modules/user.js
@@ -0,0 +1,37 @@
+import {userInfo} from '@/apis/personal'
+export default {
+    namespaced: true,
+    state: {
+        token: '',
+        userInfo: {} // 用户基本信息
+    },
+    getters: {
+        isLogin(state) {
+            console.log(state.token, !!state.token || !!localStorage.getItem('pm_token'))
+            return !!state.token || !!localStorage.getItem('pm_token')
+        }
+    },
+    mutations: {
+        setToken(state, token) {
+            localStorage.setItem('pm_token', token)
+            state.token = token
+        },
+        clearToken(state) {
+            state.token = ''
+            localStorage.removeItem('pm_token')
+        },
+        setUserInfo(state, info) {
+            state.userInfo = info || {}
+        },
+        clearUserInfo(state) {
+            state.userInfo = {}
+        }
+    },
+    actions: {
+        refreshInfo({ commit }) {
+            userInfo().then(res => {
+                commit('setUserInfo', res.data)
+            })
+        }
+    }
+}
